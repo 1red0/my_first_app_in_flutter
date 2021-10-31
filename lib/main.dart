3ba62c03202705da86dc7df5d1861dfb71309f75
1red0 <andumarius9@gmail.com>
2021-10-31 20:39:24 +0200

    commit nr.2

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
